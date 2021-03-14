6f7a801d13fa3d920d41e2aff017668d15761f62
Jon Jagger <jon@jaggersoft.com>
2021-03-14 10:58:50 +0000

    Remove redundant parentheses around if statement

--- start_point/hiker_test.go
+++ start_point/hiker_test.go
@@ -3,7 +3,7 @@ package hiker
 import ("testing")
 
 func Test_life_the_universe_and_everything(t *testing.T) {
-    if (answer() != 42) {
+    if answer() != 42 {
         t.Error("answer() != 42 as expected.")
     }
 }
